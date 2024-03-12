8bdbf5f74f902707f1b5267b236e5418df34a2f6
Muslim Gapparov <muslimgapparov@gmail.com>
2024-03-13 02:36:45 +0300

    Initial Commit

---
+++ URLProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLProject
+//
+//  Created by Муслим Гаппаров on 3/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
